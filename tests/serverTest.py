cef1a81fb1d1875e571519c498be73fb5e26b20d
Mathieson Facer <mathiesonfacer@gmail.com>
2012-04-01 20:47:39 -0700

    Just started to write this unit test module. Nothing really in there yet. Going to be learning as I go here.

--- tests/serverTest.py
+++ tests/serverTest.py
@@ -3,3 +3,12 @@ Created on Mar 21, 2012
 
 @author: mat.facer
 '''
+
+import unittest
+from doubleBarrel.server import SGServer
+
+
+class CreationTest(unittest.TestCase):
+
+    def test(self):
+        pass
