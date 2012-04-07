0f0e271c485eebcb4d7f7163c93ca01c02d5ec10
Mathieson Facer <mathiesonfacer@gmail.com>
2012-04-07 14:28:57 -0700

    Wrote the functions needed for the dynamic send and receive to work. Need to test still.

--- doubleBarrel/dynasocket.py
+++ doubleBarrel/dynasocket.py
@@ -13,15 +13,29 @@ class DynaSocket(socket.socket):
     a dynamic manner. Receive length is no longer required and is calculated
     dynamically by the send and recv functions.
     '''
-    
+
+    _defaultRecvLength = 12
+
     def send(self, message):
         '''
         Sends the message's length, then sends the message.
         '''
-        
-        
-    
+
+        # Send the length of the message first, so the receiver knows how big it will be.
+        socket.socket.send(self, str(len(message)))
+        # Wait for confirmation that the message has been received.
+        socket.socket.recv(self, self._defaultRecvLength)
+        # Send the actual message.
+        socket.socket.send(self, str(message))
+
     def recv(self):
         '''
-        Receives the message length, 
+        Receives the message length first, then receives the actual message.
         '''
+
+        # Receive the message using the default receive length.
+        messageLength = int(socket.socket.recv(self, self._defaultRecvLength))
+        # Send back confirmation that the message was received.
+        socket.socket.send(self, str(messageLength))
+        # Receive and return the actual message, using the proper length.
+        return socket.socket.recv(self, messageLength)
