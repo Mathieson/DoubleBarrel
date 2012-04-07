29c8d100e095e22669db92d834ee9bf975108a4e
Mathieson Facer <mathiesonfacer@gmail.com>
2012-04-07 13:56:33 -0700

    Accidentally renamed server when I didn't mean to. Adding a new module that will have a subclass of socket so that messages sent can be dynamic.

---
+++ doubleBarrel/dynasocket.py
@@ -0,0 +1,19 @@
+'''
+Created on 2012-04-07
+
+@author: Mat
+'''
+
+import socket
+
+
+class DynaSocket(socket.socket):
+    '''
+    This is essentially the same as a regular socket, but
+    '''
+    
+    def send(self, message):
+        pass
+    
+    def recv(self):
+        pass
